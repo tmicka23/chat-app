f283ae109310c486f79b1994377e7940db2c6509
tmicka23 <mickathp42@gmail.com>
2020-04-09 10:44:14 +0200

    enable handleStatus

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,23 +3,60 @@ import './Contact.css';
 import PropTypes from 'prop-types';
 
 
-const Contact = ({name, avatar, online}) => {
+// const Contact = ({name, avatar, online}) => {
 
-  return (
-    <div className="Contact">
-      <img src={avatar} alt={name} className="avatar"/>
-      <div>
-        <h6 className="name">
-          {name}
-        </h6>
-        <div className="status">
-        <span className={online ? "status-online" : "status-offline"}></span>
-          <span className="status-text">{online ? 'online' : 'offline'}</span>
-        </div>
-      </div>
-    </div>
-  )
+//   return (
+//     <div className="Contact">
+//       <img src={avatar} alt={name} className="avatar"/>
+//       <div>
+//         <h6 className="name">
+//           {name}
+//         </h6>
+//         <div className="status">
+//         <span className={online ? "status-online" : "status-offline"}></span>
+//           <span className="status-text">{online ? 'online' : 'offline'}</span>
+//         </div>
+//       </div>
+//     </div>
+//   )
+// }
+
+
+class Contact extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      online : props.online
+    }
+  }
+
+  handleStatus = (event) => {
+    const newStatus = this.state.online ? false : true
+    this.setState({online: newStatus})
+  }
+
+  render() {
+    return (
+      <div className="Contact">
+       <img src={this.props.avatar} alt={this.props.name} className="avatar"/>
+       <div>
+         <h6 className="name">
+           {this.props.name}
+         </h6>
+         <div className="status" id="statusContainer" onClick={this.handleStatus}>
+         <span className={this.state.online ? "status-online" : "status-offline"}></span>
+           <span className="status-text">{this.state.online ? 'online' : 'offline'}</span>
+         </div>
+       </div>
+     </div>
+    )
+  }
 }
+
+
+
+
+
 
 Contact.propTypes = {
   online: PropTypes.bool,
